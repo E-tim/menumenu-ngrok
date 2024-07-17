48e1e82522aa5adaa378863b3179489f1f543370
unknown <eesuolaayot@gmail.com>
2024-07-17 12:33:54 +0100

    jdjd

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const axios = require('axios')
+const cors = require('cors')
 
 const app = express();
 const port = process.env.PORT || 2000;
+app.use(cors()); // Use cors middleware
 
 
 app.use(express.json());
